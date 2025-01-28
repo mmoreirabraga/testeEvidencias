69ab4a270b2e7f1f6afc923cf1e458d8e83af5be
moreiraBraga <matheusmbraga94@gmail.com>
2025-01-28 18:22:49 -0300

    Segundo commit

--- src/testeEvidencias/teste/Main.java
+++ src/testeEvidencias/teste/Main.java
@@ -3,7 +3,7 @@ package testeEvidencias.teste;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Teste da aplicação");
+		System.out.println("Teste da aplicação com uso de Inteligencia Artificial");
 	}
 
 }
